22fe3ab232b21a0670f9cd0a0a6f76983300da1e
Daniil Grydin <61776829+daniilgrydin@users.noreply.github.com>
2026-01-21 11:31:30 -0800

    code handling

--- Test/test.php
+++ Test/test.php
@@ -6,17 +6,27 @@ $lines = file('test.md');
 $mode = $NONE;
 $output = "";
 foreach ($lines as $line_num => $line) {
-    if(str_starts_with($line, "###")){
-        $output .= "<h3>".substr($line,3)."</h3>";
-    }
-    elseif(str_starts_with($line, "##")){
-        $output .= "<h2>".substr($line,2)."</h2>";
-    }
-    elseif(str_starts_with($line, "#")){
-        $output .= "<h1>".substr($line,1)."</h1>";
-    }
-    elseif(strlen($line) > 0){
-        $output .= "<p>$line</p>";
+    if ($mode == $NONE) {
+        if (str_starts_with($line, "###")) {
+            $output .= "<h3>" . substr($line, 3) . "</h3>";
+        } elseif (str_starts_with($line, "##")) {
+            $output .= "<h2>" . substr($line, 2) . "</h2>";
+        } elseif (str_starts_with($line, "#")) {
+            $output .= "<h1>" . substr($line, 1) . "</h1>";
+        } elseif (str_starts_with($line, "```")) {
+            $mode = $CODE;
+        } elseif (strlen($line) > 0) {
+            $output .= "<p>$line</p>";
+        }
+    } elseif ($mode == $CODE) {
+        if(str_starts_with($line, "```")){
+            $mode = $NONE;
+        }else{
+            $text = str_replace($line, "&", "&amp;");
+            $text = str_replace($line, "<", "&lt;");
+            $text = str_replace($line, ">", "&gt;");
+            $output .= "$text\n";
+        }
     }
 }
 echo $output;
