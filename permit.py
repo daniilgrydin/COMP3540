15ef409cc86f0171bc940d3da70cfb8d835c3643
Daniil Grydin <61776829+daniilgrydin@users.noreply.github.com>
2026-01-23 09:09:36 -0800

    added a script to quickly change file permissions

---
+++ permit.py
@@ -0,0 +1,19 @@
+import os
+
+file_count = 0
+dir_count = 0
+
+def recursive_permission(dir = ""):
+    global file_count, dir_count
+    for file_path in os.listdir(dir):
+        if os.path.isdir(file_path):
+            dir_count += 1
+            os.chmod(file_path, 0o711)
+            recursive_permission(file_path)
+        else:
+            file_count += 1
+            os.chmod(file_path, 0o644)
+
+recursive_permission()
+print("Done!")
+print("Finished with", file_count, "files and", dir, "directories.")
