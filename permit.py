1e17f3a8f699a18bc370ed3ddd17a1a9797c8745
Daniil Grydin <61776829+daniilgrydin@users.noreply.github.com>
2026-01-23 09:13:52 -0800

    fixed directory omission

--- permit.py
+++ permit.py
@@ -3,9 +3,10 @@ import os
 file_count = 0
 dir_count = 0
 
-def recursive_permission(dir = ""):
+def recursive_permission(dir = "./"):
     global file_count, dir_count
     for file_path in os.listdir(dir):
+        file_path = dir + file_path
         if os.path.isdir(file_path):
             dir_count += 1
             os.chmod(file_path, 0o711)
