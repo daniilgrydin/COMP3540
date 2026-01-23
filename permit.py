1dbe118492ec7454b73833f30eab1ac66e11f4f6
Daniil Grydin <61776829+daniilgrydin@users.noreply.github.com>
2026-01-23 09:18:05 -0800

    fixed forgotten / in paths

--- permit.py
+++ permit.py
@@ -6,7 +6,7 @@ dir_count = 0
 def recursive_permission(dir = "./"):
     global file_count, dir_count
     for file_path in os.listdir(dir):
-        file_path = dir + file_path
+        file_path = dir + "/" + file_path
         if os.path.isdir(file_path):
             dir_count += 1
             os.chmod(file_path, 0o711)
