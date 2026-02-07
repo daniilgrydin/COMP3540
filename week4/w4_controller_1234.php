cd751c26dd957636ae0c9d6c725347a36d82f6ee
Daniil Grydin <61776829+daniilgrydin@users.noreply.github.com>
2026-02-07 00:30:32 -0800

    assignment 4.2

---
+++ week4/w4_controller_1234.php
@@ -0,0 +1,7 @@
+<?php
+	echo "page = " . $_POST['page'] . "<br>";
+	echo "command = " . $_POST['command'] . "<br>";
+	echo "username = " . $_POST['username'] . "<br>";
+	echo "password = " . $_POST['password'] . "<br>";
+	echo "email = " . $_POST['email'] . "<br>";
+?>
